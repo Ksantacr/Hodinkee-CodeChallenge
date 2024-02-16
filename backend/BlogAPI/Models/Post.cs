888f6829f398b25ce5f9fae620b8038cf2725a8f
Kevin Santacruz <ksantacr@espol.edu.ec>
2024-02-16 11:40:27 -0500

    fix(backend): docker configuration and documentation updated

--- backend/BlogAPI/Models/Post.cs
+++ backend/BlogAPI/Models/Post.cs
@@ -8,15 +8,15 @@ namespace BlogAPI.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         [BsonElement("id")]
-        public string Id { get; set; }
+        public required string Id { get; set; }
 
         [BsonElement("title")]
-        public string PostTitle { get; set; }
+        public required string PostTitle { get; set; }
 
         [BsonElement("content")]
         public decimal Content { get; set; }
 
         [BsonElement("image")]
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
--- backend/README.md
+++ backend/README.md
@@ -1,12 +1,30 @@
 # BlogAPI v1
 
 A CRUD API for a blog that uses Swagger and MongoDB. The API is build as a docker image.
+
 ## Build
 
 1. Ensure that you have docker installed on your machine.
-2. docker-compose build
-3. docker-compose up
-4. navigate to http://localhost:8000/swagger/index.html
+2. Open the following path in your terminal
 
+```
+cd backend\BlogAPI
+```
 
+3. Run
 
+```
+docker-compose build
+```
+
+4. Run
+
+```
+docker-compose up -d
+```
+
+5. Check API documentation
+
+```
+http://localhost:8000/swagger/index.html
+```
